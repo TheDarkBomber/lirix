6072ad3cb0672cc43eb56ddcf00118c193e98136
TheDarkBomber <thedarkbomber80@gmail.com>
2021-07-01 13:44:51 +0100

    Update conflirix script

--- mklirix/conflirix.sh
+++ mklirix/conflirix.sh
@@ -3,3 +3,4 @@ systemctl enable NetworkManager
 sed -i 's/#greeter-session=example-gtk-greeter/greeter-session=lightdm-gtk-greeter/' /etc/lightdm.conf
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's@export XDG_DATA_DIRS=$MAXX_HOME/share:$XDG_DATA_DIRS@export XDG_DATA_DIRS=$HOME/.maxxdesktop:$XDG_DATA_DIRS:/usr/share:/usr/share/local@' /opt/MaXX/etc/system.desktopenv
+pacman -R gdn gnome-shell gnome-session
