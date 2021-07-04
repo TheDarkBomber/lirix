9ca4f03b72df16b24b0bdaafee60012122211cd4
TheDarkBomber <thedarkbomber80@gmail.com>
2021-07-04 14:58:42 +0100

    It's /etc/lightdm/lightdm.conf, not /etc/lightdm.conf

--- mklirix/conflirix.sh
+++ mklirix/conflirix.sh
@@ -1,6 +1,6 @@
 systemctl enable lightdm
 systemctl enable NetworkManager
-sed -i 's/#greeter-session=example-gtk-greeter/greeter-session=lightdm-gtk-greeter/' /etc/lightdm.conf
+sed -i 's/#greeter-session=example-gtk-greeter/greeter-session=lightdm-gtk-greeter/' /etc/lightdm/lightdm.conf
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's@export XDG_DATA_DIRS=$MAXX_HOME/share:$XDG_DATA_DIRS@export XDG_DATA_DIRS=$HOME/.maxxdesktop:$XDG_DATA_DIRS:/usr/share:/usr/share/local@' /opt/MaXX/etc/system.desktopenv
 pacman -R gdm gnome-shell gnome-session
