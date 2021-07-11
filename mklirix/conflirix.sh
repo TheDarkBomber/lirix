d49e76a3a9ba5ce5002aea8788a60aae4d1cb1c7
TheDarkBomber <thedarkbomber80@gmail.com>
2021-07-11 12:58:51 +0100

    Make Lirix default GRUB entry name

--- mklirix/conflirix.sh
+++ mklirix/conflirix.sh
@@ -2,5 +2,7 @@ systemctl enable lightdm
 systemctl enable NetworkManager
 sed -i 's/#greeter-session=example-gtk-greeter/greeter-session=lightdm-gtk-greeter/' /etc/lightdm/lightdm.conf
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+sed -i 's/GRUB_DISTRIBUTOR="Arch"/GRUB_DISTRIBUTOR="Lirix"/' /etc/default/grub
+sed -i 's/OS="${GRUB_DISTRIBUTOR} Linux"/OS="${GRUB_DISTRIBUTOR}"/' /etc/grub.d/10_linux
 sed -i 's@export XDG_DATA_DIRS=$MAXX_HOME/share:$XDG_DATA_DIRS@export XDG_DATA_DIRS=$HOME/.maxxdesktop:$XDG_DATA_DIRS:/usr/share:/usr/share/local@' /opt/MaXX/etc/system.desktopenv
 pacman -R gdm gnome-shell gnome-session
