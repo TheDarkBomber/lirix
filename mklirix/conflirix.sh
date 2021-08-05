be12ef3065ebf87a91130c7eb6409c0e88be2a9e
TheDarkBomber <thedarkbomber80@gmail.com>
2021-08-05 17:01:16 +0100

    Finally, a login background

--- mklirix/conflirix.sh
+++ mklirix/conflirix.sh
@@ -6,5 +6,6 @@ sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's/GRUB_DISTRIBUTOR="Arch"/GRUB_DISTRIBUTOR="Lirix"/' /etc/default/grub
 sed -i 's/OS="${GRUB_DISTRIBUTOR} Linux"/OS="${GRUB_DISTRIBUTOR}"/' /etc/grub.d/10_linux
 echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+sed -i 's@#background=@background=/opt/MaXX/share/wallpapers/sgi-startup-bg.png@' /etc/lightdm/lightdm-gtk-greeter.conf
 sed -i 's@export XDG_DATA_DIRS=$MAXX_HOME/share:$XDG_DATA_DIRS@export XDG_DATA_DIRS=$HOME/.maxxdesktop:$XDG_DATA_DIRS:/usr/share:/usr/share/local@' /opt/MaXX/etc/system.desktopenv
 pacman -R gdm gnome-shell gnome-session
