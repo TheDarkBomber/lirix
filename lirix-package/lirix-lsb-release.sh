16dd975ad789062a32d97a8445e84386b4329729
TheDarkBomber <thedarkbomber80@gmail.com>
2021-08-26 12:53:53 +0100

    Update LSB Release file

--- lirix-package/lirix-lsb-release.sh
+++ lirix-package/lirix-lsb-release.sh
@@ -2,5 +2,6 @@
 lsbver=$(head -n 1 /etc/lsb-release)
 echo $lsbver > /etc/lsb-release
 echo "DISTRIB_ID=Lirix" >> /etc/lsb-release
-echo "DISTRIB_RELEASE=$(cat /etc/lirix-release | awk '{print $2}')" >> /etc/lsb-release
-echo "DISTRIB_DESCRIPTION=\"$(cat /etc/lirix-release)\"" >> /etc/lsb-release
+echo "DISTRIB_RELEASE=\"$(cat /etc/lirix-release | awk '{print $2}'), rolling\"" >> /etc/lsb-release
+echo "DISTRIB_DESCRIPTION=\"Lirix $(cat /etc/lirix-release | awk '{print $2}')\"" >> /etc/lsb-release
+echo "DISTRIB_CODENAME=$(cat /etc/lirix-release | awk '{print $2}')" >> /etc/lsb-release
